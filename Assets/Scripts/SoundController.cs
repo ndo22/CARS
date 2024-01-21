cf6d7ee617936d564c8f4c3e3372215d7b7bfb0b
Filip Tomes <filiptomes@protonmail.com>
2024-01-21 17:37:15 +0100

    volume slider

---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundController : MonoBehaviour
+
+{
+    [SerializeField] Slider volumeSlider;
+
+    void Start()
+    {
+        if(!PlayerPrefs.HasKey("musicVolume")) {
+            PlayerPrefs.SetFloat("musicVolume", 0.5);
+            Load();
+        } 
+    }
+    public void ChangeVolume ()
+    {
+        AudioListener.volume = volumeSlider.value;
+        Save();
+    }
+
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+    }
+}
